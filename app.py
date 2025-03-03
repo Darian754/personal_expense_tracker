90410be48d8e98b531f968ac09aa2c5e0faf73cd
Darian754 <darianc754@gmail.com>
2025-03-03 19:06:32 +0200

    added basic structure of the app and added a html file.

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+
+expenses = []
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Hello, this is your Personal Expense Tracker!"
+    if request.method == 'POST':
+        # Get form data
+        date = request.form['date']
+        category = request.form['category']
+        amount = request.form['amount']
+        description = request.form['description']
+
+        # (for now, just storing in a list)
+        expenses.append({'date': date, 'category': category, 'amount': amount, 'description': description})
+
+    return render_template('index.html', expenses=expenses)
 
 if __name__ == '__main__':
     app.run(debug=True)
