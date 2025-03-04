e5498f2c98d5a511527826aaf7a3fa72f2e9011c
darian754 <darianc754@gmail.com>
2025-03-04 20:03:36 +0200

    Fixed app.py and reset project

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, this is your Personal Expense Tracker!"
-=======
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -27,7 +18,6 @@ def home():
         expenses.append({'date': date, 'category': category, 'amount': amount, 'description': description})
 
     return render_template('index.html', expenses=expenses)
->>>>>>> 90410be48d8e98b531f968ac09aa2c5e0faf73cd
 
 if __name__ == '__main__':
     app.run(debug=True)
